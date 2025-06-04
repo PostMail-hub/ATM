f80779b4c2ed5c4e2e16acca2a6793b8ef2d8c62
PostMail-hub <gmlals2949@naver.com>
2025-06-04 17:03:03 +0900

    feat : 입력 필드 숫자 제한 및 입출금 기능 연동
    - 입력 필드 숫자만 입력 가능하도록 설정 및 입출금 기능 연결

--- Assets/Scripts/Bank/PopupBank.cs
+++ Assets/Scripts/Bank/PopupBank.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PopupBank : MonoBehaviour
@@ -17,6 +18,9 @@ public class PopupBank : MonoBehaviour
     private UserData userData;
 
     public UserMoneyFormatter userMoneyFormatter;
+
+    public InputField DepositInputField;
+    public InputField WithdrawInputField;
 
     // 오브젝트가 없을 경우에 아래와 같은 이름의 오브젝트를 자동으로 참조
     private void Awake()
@@ -56,6 +60,24 @@ public class PopupBank : MonoBehaviour
             if (infoObject != null)
             {
                 userMoneyFormatter = infoObject.GetComponent<UserMoneyFormatter>();
+            }
+        }
+
+        if (DepositInputField == null)
+        {
+            GameObject depositInput = GameObject.Find("DepositInputField");
+            if (depositInput != null)
+            {
+                DepositInputField = depositInput.GetComponent<InputField>();
+            }
+        }
+
+        if (WithdrawInputField == null)
+        {
+            GameObject withdrawInput = GameObject.Find("WithdrawInputField");
+            if (withdrawInput != null)
+            {
+                WithdrawInputField = withdrawInput.GetComponent<InputField>();
             }
         }
     }
@@ -99,37 +121,61 @@ public class PopupBank : MonoBehaviour
         SwitchPanel(ATMPanel);
     }
 
+    // 버튼에 할당된 amount 값을 불러와서 그 금액만큼 현금을 - 해주고 은행 잔고를 + 해준다.
     public void Deposit(int amount)
     {
         UserData userData = GameManager.Instance.userData;
 
+        // 만약 현재 보유 중인 현금이 은행에 넣어줄 잔고보다 크거나 같다면
         if (amount <= userData.Cash)
         {
-            userData.Cash -= amount;
-            userData.AccountBalance += amount;
+            userData.Cash -= amount;             // 현재 현금을 지정된 값 만큼 빼준다.
+            userData.AccountBalance += amount;   // 은행 잔고를 지정된 값 만큼 증가 시켜준다.
         }
+        // 금액이 부족하다면
         else
         {
             Debug.Log("당신은" + amount + "만큼의 현금을 가지고 있지 않아 입금에 실패했습니다!");
         }
 
-        userMoneyFormatter.Refresh();
+        userMoneyFormatter.Refresh();           // UI에 변경 사항을 즉시 적용
     }
 
+    // 버튼에 할당된 amount 값을 불러와서 그 금액만큼 은행 잔고를 - 해주고 현금을 + 해준다.
     public void Withdrawal(int amount)
     {
         UserData userData = GameManager.Instance.userData;
 
+        // 만약 현재 은행 잔고가 현금으로 인출할 금액보다 크거나 같다면
         if (amount <= userData.AccountBalance)
         {
-            userData.AccountBalance -= amount;
-            userData.Cash += amount;
+            userData.AccountBalance -= amount;      // 은행 잔고를 지정된 값 만큼 증가 시켜준다.
+            userData.Cash += amount;                // 현재 현금을 지정된 값 만큼 빼준다.
         }
+        // 금액이 부족하다면
         else
         {
             Debug.Log("은행에" + amount + "만큼의 금액을 가지고 있지 않아 출금에 실패했습니다!");
         }
 
-        userMoneyFormatter.Refresh();
+        userMoneyFormatter.Refresh();              // UI에 변경 사항을 즉시 적용
+    }
+
+    // 직접 입력한 값이 문자열이니 이를 int로 변환하여 Deposit 에 넣어준다.
+    public void TryDeposit()
+    {
+        if (int.TryParse(DepositInputField.text, out int amount))
+        {
+            Deposit(amount);  // 입금 메서드 호출
+        }
+    }
+
+    // 직접 입력한 값이 문자열이니 이를 int로 변환하여 Withdrawal 에 넣어준다.
+    public void TryWithdraw()
+    {
+        if (int.TryParse(WithdrawInputField.text, out int amount))
+        {
+            Withdrawal(amount);  // 출금 메서드 호출
+        }
     }
 }
